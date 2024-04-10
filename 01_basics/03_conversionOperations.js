618b30291ad7ca24085e2b756a752e22180dcbd6
Jay Govind Kumar <156842524+Jay-Govind@users.noreply.github.com>
2024-04-10 06:57:23 +0000

    conversion in js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let marks1 = "99";
+
+console.log(typeof marks);
+
+let valueInNumber1 = Number(marks);
+
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+/*
+    "33"    ==> 33
+    "33abd" ==> NaN
+    true    ==> 1;
+    false   ==> 0;
+*/
+
+let marks2 = 99;
+
+console.log(typeof marks);
+
+let valueInNumber2 = String(marks);
+
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
