98c850b92285a60499aad5a5f7e7e5c82111ed78
Jay Govind Kumar <156842524+Jay-Govind@users.noreply.github.com>
2024-04-10 05:38:40 +0000

    1st day complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 43743284;
+let accountName =  "Jay govind";
+var accountEmail = "govind@google.com";
+let accountState;
+
+/* prefer not to use var, use let  */
+
+console.table([accountId, accountEmail, accountName, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict" // use all JS code as newer standard
+
+alert(3+3);
