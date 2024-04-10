5a613e1dcb745a44015818a2609d172048259165
Jay Govind Kumar <156842524+Jay-Govind@users.noreply.github.com>
2024-04-10 06:44:38 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,28 @@
 "use strict" // use all JS code as newer standard
 
-alert(3+3);
+// alert(3+3);  // we are using nodejs, not browser
+
+console.log(3+3); // code readability should be high
+
+let name = "Jay Govind";
+let age = 18;
+let isLogged = false;
+
+/*
+            Data Types
+    number      ==> 2 to power 53
+    bigint
+    string      ==> ""
+    boolean     ==> true / false
+    null        ==> standalone value
+    undefined   ==> not assigned
+    symbol      ==> unique
+
+
+    object
+*/
+
+console.log(typeof age);
+console.log(typeof null);
+
+// null is object not a datatype
