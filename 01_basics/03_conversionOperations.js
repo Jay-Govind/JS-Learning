8259205b909db14f3f99f29187a5f9f3693daf6b
Jay Govind Kumar <156842524+Jay-Govind@users.noreply.github.com>
2024-04-10 07:26:07 +0000

    conversion update

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
-let marks1 = "99";
+// let marks1 = "99";
 
-console.log(typeof marks);
+// console.log(typeof marks);
 
-let valueInNumber1 = Number(marks);
+// let valueInNumber1 = Number(marks);
 
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
 
 /*
     "33"    ==> 33
@@ -14,11 +14,51 @@ console.log(valueInNumber1);
     false   ==> 0;
 */
 
-let marks2 = 99;
+// let marks2 = 99;
 
-console.log(typeof marks);
+// console.log(typeof marks);
 
-let valueInNumber2 = String(marks);
+// let valueInNumber2 = String(marks);
 
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+
+
+// *****************************  Operations ***********************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(3+2);    //==>5
+// console.log(3-2);    //==>1
+// console.log(2*2);    //==>4
+// console.log(3**2);   //==>9
+// console.log(2/2);    //==>1
+// console.log(2%2);    //==>0
+
+// let str1 = "Jay";
+// let str2 = " Govind"
+
+// let str3 = str1 + str2;
+// console.log(str3);          //Jay Govind
+
+// console.log("1" + 2);       //==>12
+// console.log(1 + "2");       //==>12
+// console.log("1" + "2");     //==>12
+// console.log("1" + 2 + 2);   //==>123
+// console.log(1 + 2 + "2");   //==>32
+
+// console.log(+true);          //==>1
+// console.log(+"");            //==>0
+
+
+let num1, num2, num3;
+num1= num2 = num3 = 2 + 2;      // prefer not to use, soo tricky and confusing
+
+let gameCounter = 100;
+++gameCounter;      //Pre fix
+gameCounter++;      //Post fix
+
+/* Read Documentation */
