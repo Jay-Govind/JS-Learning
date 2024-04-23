518419c160b7421f9ade8c43e063af0e3a666ae3
Jay-Govind <govind.iq@gmail.com>
2024-04-23 14:27:59 +0530

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,54 @@
+// function name () {
+//       console.log("J");
+//       console.log("a");
+//       console.log("y");
+//       console.log(" ");
+//       console.log("G");
+//       console.log("o");
+//       console.log("v");
+//       console.log("i");
+//       console.log("n");
+//       console.log("d");
+// } name();
+
+
+
+// function add ( number1, number2 ) {       // --> parameters
+//       console.log(number1 + number2);     // --> 5
+// } 
+// const sum = add(2, 3);        // --> arguments
+// console.log(`Results are ${sum}`);  // --> undefined
+/* Function does not return any value, if we don't write return statement */
+
+
+
+// function loginUsername (user = "Unknown Coder") {
+//       if (!user) {
+//             console.log("First Enter your name");
+//             return;
+//       }
+//       return `${ user } just logged in`;
+// }
+
+// console.log(loginUsername("jay govind"));    // --> jay govind just logged in
+// console.log(loginUsername());                // --> Unknown Coder just logged in
+
+
+
+function calculateCardPrice(...price) {    // --> rest operator (depend on place of usage)
+      return price;
+}
+// console.log(calculateCardPrice(100, 200, 300, 400, 500));   // --> return an array
+
+
+
+const user = {
+      username: "Govind",
+      price: 199
+}
+
+function handleObject(anyObj) {
+      console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`);
+}
+// handleObject(user);
+handleObject( { username: "Jay Govind", price: 299 } );
