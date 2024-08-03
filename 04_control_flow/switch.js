cb16252ebf3f9d4009fbd5e90606f6a0dfe7ca09
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-03 23:35:52 +0530

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,60 @@
+// switch (key) {
+//   case value:
+//     // code block
+//     break;
+//   case value2:
+//     // code block
+//     break;
+//   default:
+//   // code block
+// }
+
+
+
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    console.log("Invalid Month");
+}
+
+
+// break is used to break the loop
