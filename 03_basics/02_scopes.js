8c2802d1c3571a1e7ae7002142ab3dc3fb1614e6
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-03 21:54:02 +0530

    Scope in details

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,12 +9,44 @@
 
 // console.log(c);
 
-//--------------------------------
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
 let a = 100;
 const b = 200;
 var c = 300;
 if (true) {
-  console.table([a, b, c]);
+  // console.table([a, b, c]);
 }
 
 /* here a, b and c are declared in global scope, so they will be printed either inside or outside any block. We can also initialise them in the if block also */
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one() {
+  let username = "Govind";
+  function two() {
+    let website = "Youtube";
+    console.log(username + " " + website);
+  }
+  two();
+  // console.log(website);    --> give error  (website is not defined), it is defined only in function two
+}
+
+// console.log(username);   --> give error  (username is not defined), it is defined only in function one
+
+one(); // call the function
+
+// ++++++++++++++++++++++++ Interesting Concept +++++++++++++++++++++
+console.log(addtwo(10));
+function addtwo(num) {
+  return num + 2;
+}
+
+console.log(addtwo(10)); // this will give error, bcoz function is not defined
+const result = function addTwo(num) {
+  return num + 2;
+};
+
+/* we can call the function before defining it, if the function is defined in the global scope.
+But we can't call the function before defining it, if it is defined in the local scope i.e stored inside a variable */
+
+// this is closure concept and function hoisting
