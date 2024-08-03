8298f1b7723ad5e0d0399368fc8cb37b24682dde
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-04 00:17:32 +0530

    while loop & do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,42 @@
+// while loop
+
+// let index = 0;
+
+// while (index <=10) {
+//     console.log(`index : ${index}`);
+//     index = index + 2;
+    
+// }
+
+
+// let myArr = ["jay", "govind", "kumar"];
+
+// let index =0;
+// while (index < myArr.length) {
+//     console.log(`value is ${myArr[index]}`);
+//     index = index + 1;
+    
+// }
+
+
+
+
+// ++++++++++++++++++++++++  do while ++++++++++++++++++++++++++++++
+
+// let score = 1;
+// do {
+//     console.log(`Score is ${score}`);
+//     score = score + 1;
+    
+// } while (score <= 10);
+
+
+
+// let score = 11;
+// do {
+//     console.log(`Score is ${score}`);
+//     score = score + 1;
+    
+// } while (score <= 10);
+
+// code executes atleast once
