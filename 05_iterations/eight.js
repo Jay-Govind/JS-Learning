172aef6e72ade97c6754b92e8990b6133bf0220c
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-04 13:42:33 +0530

    reduce method

---
+++ 05_iterations/eight.js
@@ -0,0 +1,11 @@
+// +++++++++++++++++++ reduce ++++++++++++++++++++++
+
+const myNums = [1, 2, 3, 4];
+
+const myTotal = myNums.reduce ( (acc, currVal) => {
+    console.log(`acc : ${acc} currVal : ${currVal}`);
+    
+    return acc + currVal;
+}, 0 )
+
+console.log(myTotal);
