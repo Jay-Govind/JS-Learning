cd10ea770254ae62d17ae4606fcd7b0a2b2a2836
Jay Govind Kumar <govind.iq@gmail.com>
2024-08-04 13:22:45 +0530

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "python", "java", "swift"];
+
+// const value = coding.forEach((item) => {
+//   console.log(item);
+//   return item;       
+// });
+
+// console.log(value);      // --> undefined
+
+
+
+
+
+// +++++++++++++++++++++++++++++ filter +++++++++++++++++++++++++++++++
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNums.filter( (num) =>  num> 4  )
+// console.log(newNums);   // --> [5, 6, 7, 8, 9, 10]
+
+
+
+// const newNums2 = myNums.filter( (num) =>  { 
+//     return num> 4 
+// } )
+// console.log(newNums2);   // --> [5, 6, 7, 8, 9, 10]
+
+
+
+// ------------------------------------------
+// const newNums3 = []
+
+// myNums.forEach( (item) => {
+
+//     if (item > 4) {
+//         newNums3.push(item);
+//     }
+// })
+// console.log(newNums3);
+
+
+
+
